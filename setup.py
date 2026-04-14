1649cdbb14885fed3b723c26054a5f9608811d62
Paul Schlack <schlack@8sgqfd4.fe.hhi.de>
2026-04-14 20:00:17 +0200

    Apply splatkit packaging wrapper patch

--- setup.py
+++ setup.py
@@ -3,32 +3,35 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
 #
 
+from pathlib import Path
+
 from setuptools import setup
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-import os
-os.path.dirname(os.path.abspath(__file__))
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+ROOT = Path(__file__).resolve().parent
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    packages=["diff_gaussian_rasterization"],
     ext_modules=[
         CUDAExtension(
             name="diff_gaussian_rasterization._C",
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+                "cuda_rasterizer/rasterizer_impl.cu",
+                "cuda_rasterizer/forward.cu",
+                "cuda_rasterizer/backward.cu",
+                "rasterize_points.cu",
+                "ext.cpp",
+            ],
+            extra_compile_args={
+                "nvcc": [f"-I{ROOT / 'third_party' / 'glm'}"],
+            },
+        ),
+    ],
+    cmdclass={"build_ext": BuildExtension},
 )
